26ea33bc06349d326c9771b9ff9ba8a0c67f4503
Roland Moriz <roland@moriz.de>
2015-07-03 13:35:06 +0200

    updated gems + more cleanup

--- Gemfile
+++ Gemfile
@@ -1,7 +1,36 @@
 source 'https://rubygems.org'
 
-gem 'foodcritic', '>= 3.0.3'
-gem 'berkshelf', '~> 2.0'
-gem 'chefspec', '~> 3.0'
+gem 'chef', '~> 12.0'
 
-gem 'rubocop'
+group :lint do
+  gem 'foodcritic', '~> 4.0'
+  gem 'rubocop', '~> 0.29.1'
+  gem 'rake'
+end
+
+group :unit do
+  gem 'berkshelf', '~> 3.1'
+  gem 'chefspec',  '~> 4.2'
+  gem 'chef-handler-datadog'
+end
+
+group :kitchen_common do
+  gem 'test-kitchen', '~> 1.4'
+  gem 'kitchen-digitalocean'
+  gem 'chef-zero'
+end
+
+group :kitchen_vagrant do
+  gem 'kitchen-vagrant'
+end
+
+group :development do
+  gem 'ruby_gntp'
+  gem 'growl'
+  gem 'rb-fsevent'
+  gem 'guard', '~> 2.6'
+  gem 'guard-kitchen'
+  gem 'guard-foodcritic'
+  gem 'guard-rspec'
+  gem 'guard-rubocop'
+end
--- providers/virtualenv.rb
+++ providers/virtualenv.rb
@@ -33,7 +33,7 @@ action :create do
       group new_resource.group if new_resource.group
     end
     Chef::Log.info("Creating virtualenv #{new_resource} at #{new_resource.path}")
-    interpreter = new_resource.interpreter ? " --python=#{new_resource.interpreter}" : ""
+    interpreter = new_resource.interpreter ? " --python=#{new_resource.interpreter}" : ''
     execute "#{virtualenv_cmd}#{interpreter} #{new_resource.options} #{new_resource.path}" do
       user new_resource.owner if new_resource.owner
       group new_resource.group if new_resource.group
@@ -69,12 +69,12 @@ def virtualenv_cmd
   if ::File.exist?(node['python']['virtualenv_location'])
     node['python']['virtualenv_location']
   else
-    "virtualenv"
+    'virtualenv'
   end
 end
 
 private
 
 def exists?
-  ::File.exist?(current_resource.path) && ::File.directory?(current_resource.path) \ && ::File.exist?("#{current_resource.path}/bin/activate")
+  ::File.exist?(current_resource.path) && ::File.directory?(current_resource.path) && ::File.exist?("#{current_resource.path}/bin/activate")
 end
